ee401736cfddd056612fcd33cc6dd2898e453aa8
Gustavo Briceno <gustavobrcn@hotmail.com>
2019-11-25 23:52:18 -0500

    Added trainer team and pokemon display

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const trainerForm = document.getElementById('trainer-form')
 const pokemonForm = document.getElementById('pokemon-form')
 const trainerIcon = document.getElementById('trainer-icons')
 const selectedTrainer = document.getElementsByClassName('trainers')
+const displayTeam = document.getElementById('display-team')
+const pokemonTeam = document.getElementById('pokemon-team')
 let trainer
 
 startBtn.addEventListener('click', () => {
@@ -16,12 +18,46 @@ trainerForm.addEventListener('submit', (e) => {
   trainer = new Trainer(name)
   trainerForm.style.display = 'none'
   trainerIcon.style.display = 'block'
+  document.getElementById('trainer-name').innerHTML = `Trainer ${name}'s Team`
 })
 
-pokemonForm.addEventListener('submit', () => {
+for (let i = 1 ; i < 9; i++) {
+  document.getElementById(`trainer${i}`).addEventListener('click', () => {
+    trainerIcon.style.display = 'none'
+    trainerImg = document.getElementById(`trainer${i}`).src
+    trainer.setIcon(trainerImg)
+    document.getElementById('trainer-pic').src = trainerImg
+    displayTeam.style.display = 'block'
+    startBtn.style.display = 'none'
+  })
+
+}
+let count = 0
+pokemonForm.addEventListener('submit', async (e) => {
+  e.preventDefault()
   const pokemonName =  document.getElementById('pokemon-input').value
   const pokemon = new Pokemon(pokemonName)
+  await pokemon.init()
   trainer.addPokemon(pokemon)
+  const image = document.createElement('img')
+  image.src = pokemon.image
+  image.dataset.index = count++
+  pokemonTeam.appendChild(image)
+
 })
 
-// make pokemon form set ID for input as pokemon-input
+pokemonTeam.addEventListener('click', (e) => { //current target is the pokemon team - what the listener is attached to
+  console.log(e.target);                       //target is the thing that caused the event (img)
+  console.log(e.currentTarget);
+  if (e.target != e.currentTarget) {
+  const pokemonIndex = e.target.dataset.index
+  let pokemon = trainer.all()[pokemonIndex]
+  pokemonStats = document.getElementsByClassName('pokemon-info')
+  pokemonStats[0].src = pokemon.image
+  pokemonStats[1].innerHTML = `Name: ${pokemon.name}`
+  pokemonStats[2].innerHTML = `Def: ${pokemon.def}`
+  pokemonStats[3].innerHTML = `Abilities: ${pokemon.abilities[0]}, ${pokemon.abilities[1]}`
+  pokemonStats[4].innerHTML = `HP: ${pokemon.hp}`
+  }
+  e.stopPropagation() //
+})
--- pokemon.js
+++ pokemon.js
@@ -1,38 +1,30 @@
-class Pokemon { 
+class Pokemon {
     constructor(pokemonName) {
-      this.queryPokemonAPI(pokemonName).then(pokemon => {
-        this.name = data.name
-        this.hp = data.stats[5].base_stat
-        this.atk = data.stats[4].base_stat
-        this.def = data.stats[3].base_stat
-        this.abilities = [data.abalities[0].ability.name, data.abalities[1].ability.name]
-        this.priority = null
-        this.power = null
-        this.accuracy = null
-        this.image = null
-      });
-    }
-  
-    addAbilities(array) {
-      this.abilites = [...array]
-  
-    }
-  
-    static queryPokemonAPI = async (id) => {
+      this._name = pokemonName
+  }
+
+  init() {
+  return Pokemon.queryPokemonAPI(this._name).then(pokemon => {
+      this.name = pokemon.name
+      this.hp = pokemon.stats[5].base_stat
+      this.atk = pokemon.stats[4].base_stat
+      this.def = pokemon.stats[3].base_stat
+      this.abilities = [pokemon.abilities[0].ability.name, pokemon.abilities[1].ability.name]
+      this.image = pokemon.sprites.front_default
+      // this.priority = null
+      // this.power = null
+      // this.accuracy = null
+    });
+  }
+
+  static queryPokemonAPI = async (id) => {
       const data = await fetch(`https://fizal.me/pokeapi/api/v2/id/${id}.json`)
         .then(res => res.json())
         .catch(error => {
           return fetch(`https://fizal.me/pokeapi/api/v2/name/${id}.json`)
             .then(res => res.json())
-        });
-      console.log(data)
-          
-      return data;
-  
-    
-    
-  
-    }
-}
+          });
 
-  
+          return data;
+        }
+}
